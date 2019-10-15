5da9bf31ca5e0e0180e5227b886d346f021e0f31
hk3cu <hk3cu@virginia.edu>
2019-10-14 22:23:51 -0400

    Update register.php

--- directoryads/register.php
+++ directoryads/register.php
@@ -319,6 +319,43 @@
                   <option>Alabama</option> <!-- Add the states here -->
                   <option>Alaska</option>
                   <option>Arizona</option>
+                  <option>Arkansas</option> 
+                  <option>California</option>
+                  <option>Colorado</option>
+                  <option>Connecticut</option>
+                  <option>Delaware</option>
+                  <option>Florida</option>
+                  <option>Georgia</option> 
+                  <option>Hawaii</option>
+                  <option>Idaho</option>
+                  <option>Illinois</option> 
+                  <option>Indiana</option>
+                  <option>Iowa</option>
+                  <option>Kansas</option>
+                  <option>Kentucky</option>
+                  <option>Louisiana</option>
+                  <option>Maine</option> 
+                  <option>Maryland</option> 
+                  <option>Massaschusetts</option>
+                  <option>Michigan</option>
+                  <option>Minnesota</option> 
+                  <option>Mississippi</option>
+                  <option>Missouri</option>
+                  <option>Montana</option>
+                  <option>Nebraska</option>
+                  <option>Nevada</option>
+                  <option>New Hampshire</option> 
+                  <option>New Jersey</option> 
+                  <option>New Mexico</option>
+                  <option>New York</option>
+                  <option>North Carolina</option> 
+                  <option>North Dakota</option>
+                  <option>Ohio</option>
+                  <option>Oklahoma</option>
+                  <option>Oregon</option>
+                   
+                  
+                 
                 </select>
                 <span class="error"> <?php echo $errState;?> </span>
               </div>
