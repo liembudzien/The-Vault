00fee73700d32ffd9ec0d9678d86bc6c2c36d4b1
hk3cu <hk3cu@virginia.edu>
2019-10-28 23:13:17 -0400

    Update login.php

--- directoryads/login.php
+++ directoryads/login.php
@@ -14,14 +14,7 @@
     <link rel="stylesheet" href="css/owl.carousel.min.css">
     <link rel="stylesheet" href="css/owl.theme.default.min.css">
 
-    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
-
-    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
-
-    <link rel="stylesheet" href="css/aos.css">
-    <link rel="stylesheet" href="css/rangeslider.css">
-
-    <link rel="stylesheet" href="css/style.css">
+    
     
   </head>
   <body>
