65a834a80077474d30bc1efcd2e84813079355fc
hk3cu <hk3cu@virginia.edu>
2019-10-28 23:12:48 -0400

    Update index.php

--- directoryads/index.php
+++ directoryads/index.php
@@ -9,11 +9,11 @@
     <link href="https://fonts.googleapis.com/css?family=Nanum+Gothic:400,700,800" rel="stylesheet">
     <link rel="stylesheet" href="fonts/icomoon/style.css">
 
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!--<link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/magnific-popup.css">
     <link rel="stylesheet" href="css/jquery-ui.css">
     <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">-->
 
     <link rel="stylesheet" href="css/bootstrap-datepicker.css">
 
